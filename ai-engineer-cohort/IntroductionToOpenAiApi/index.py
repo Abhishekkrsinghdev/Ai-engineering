6ec835f92284dd9223ef8efb99b06a0ab98adddf
Abhishekkrsinghdev <hrithiksinghrocks31@gmail.com>
2026-08-04 15:46:53 +0530

    added open ai api call code

---
+++ ai-engineer-cohort/IntroductionToOpenAiApi/index.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+client=OpenAI()
+
+response=client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {
+            "role":"user",
+            "content":"What is the capital of France?"
+        }
+    ]
+)
+
+print(response.choices[0].message.content)
